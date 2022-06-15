5d67b449f81f2b79ea3a65ee51e84514410ca3a7
vinicius <vini.soares.martins@gmail.com>
2022-06-15 15:20:07 -0300

    Add styled components to project

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,17 @@
 import Head from 'next/head'
-import { Container, Row, Card, Button } from 'react-bootstrap'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <Container className="md-container">
+    <>
       <Head>
         <title>Pet Shop</title>
       </Head>
-      <Navbar/>
-      <HeroSection/>
-      <Footer/>
-    </Container>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </>
   )
 }
