2cd0e1e69bc71a78c32d619e8b51109fde9e22ed
vinicius <vini.soares.martins@gmail.com>
2022-06-15 20:48:02 -0300

    Fix to deploy in vercel

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
+        <meta name="description" content='pet-shop is a company that sells pet products' />
         <title>Pet Shop</title>
       </Head>
       <Navbar />
--- pages/minha-conta/index.jsx
+++ pages/minha-conta/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { Container, Row, Card, Button } from 'react-bootstrap'
 import Navbar from '/components/Navbar'
 import HeroSection from '/components/HeroSection'
 import Footer from '/components/Footer'
