23a45b2d3877ba8501c4263014520f9661e4de9c
ericarfs <ericaribeiro@usp.br>
2022-06-17 15:21:25 -0300

    update

--- pages/recuperar-senha.jsx
+++ pages/recuperar-senha.jsx
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 import Head from 'next/head'
 import Navbar from '/components/Navbar'
 import RecuperarSenha from '/components/RecuperarSenha'
 
 import  {ContainerRow, Div40} from '/style/pagesStyles.jsx'
-import { Button, ButtonContainer } from '../components/style'
 
 export default function Home() {
   return (
@@ -20,29 +18,4 @@ export default function Home() {
       </ContainerRow>
       </>
   )
-
-=======
-import Head from 'next/head'
-import Navbar from '/components/Navbar'
-import RecuperarSenha from '/components/RecuperarSenha'
-
-import  {ContainerRow, Div40} from '/style/pagesStyles.jsx'
-import { Button, ButtonContainer } from '../components/style'
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Pet Shop</title>
-      </Head>
-      <Navbar/>
-      <ContainerRow>
-        <Div40>
-          <RecuperarSenha/>
-        </Div40>       
-      </ContainerRow>
-      </>
-  )
-
->>>>>>> 0bcc9cc4ab0b107659e9dc2db7edd98d76d0cb48
 }
