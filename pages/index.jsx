e27268f075ceac2bb5b89b4de4e4812081964320
ericarfs <ericaribeiro@usp.br>
2022-06-19 12:46:50 -0300

    atualizar index

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,11 +4,15 @@ import HeroSection from '../components/HeroSection'
 import Footer from '../components/Footer'
 import { useEffect } from 'react'
 
+import { useRouter } from 'next/router'
+
 export default function Home() {
+  const router = useRouter();
   useEffect(() => {
     if(localStorage.getItem('isLoggedIn') == null){
       localStorage.setItem('isLoggedIn',''); 
     }
+    router.push('/');
   }, []);
 
   return (
