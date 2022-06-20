8ac373480c8c632fa2d3493eb63536e956b65240
viniciussm07 <vini.soares.martins@gmail.com>
2022-06-20 10:25:25 -0300

    creating animal folder and dynamic animal page

---
+++ pages/animal/[animal].jsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import Footer from '../components/Footer'
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/router'
+
+export default function Animal() {
+  const router = useRouter();
+  useEffect(() => {
+    if(localStorage.getItem('isLoggedIn') == null){
+      localStorage.setItem('isLoggedIn',''); 
+    }
+    router.push('/');
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Pet Shop</title>
+      </Head>
+      <Navbar />
+      <Footer/>
+    </>
+  )
+}
