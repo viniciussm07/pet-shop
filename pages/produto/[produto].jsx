e77d21d266519dcd230a61b0e1b55025025ba200
viniciussm07 <vini.soares.martins@gmail.com>
2022-06-20 15:49:09 -0300

    add footer on page product

--- pages/produto/[produto].jsx
+++ pages/produto/[produto].jsx
@@ -1,12 +1,14 @@
 import Navbar from '../../components/Navbar'
 import Layout from '../../components/Produto/ProdutoLayout'
 import {useRouter} from 'next/router'
+import Footer from '../../components/Footer'
 
 export default function produto(){
     return(
         <>
             <Navbar/>
             <Layout/>
+            <Footer/>
         </>
     )
 }
