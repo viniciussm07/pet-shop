656752b3368e6b59ebe5c135463e35c414ce49a8
vinicius <vini.soares.martins@gmail.com>
2022-07-14 07:18:50 -0300

    criando pastas para editar produtos

---
+++ src/pages/admin/lista-produtos/editar-produtos.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../../components/Navbar";
+import Footer from "../../../components/Footer";
+import Editar from "../../../components/Admin/Editar"
+
+export default function EditarClientes() {
+  return (
+    <>
+      <Navbar />
+        <Editar/>
+      <Footer />
+    </>
+  );
+}
