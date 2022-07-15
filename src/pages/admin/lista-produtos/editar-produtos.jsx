95b209a841eebb31d5b502dfa8ac6dd8a03aa578
vinicius <vini.soares.martins@gmail.com>
2022-07-15 09:24:05 -0300

    fix

--- src/pages/admin/lista-produtos/editar-produtos.jsx
+++ src/pages/admin/lista-produtos/editar-produtos.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
 import Editar from "../../../components/Admin/Editar"
 
-export default function EditarClientes() {
+export default function EditarProdutos() {
   return (
     <>
       <Navbar />
